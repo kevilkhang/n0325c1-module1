9dcdb73f3af800c3437dd538b7c7a22fce87d74f
kevilkhang <kevilkhang@gmail.com>
2025-03-05 14:30:05 +0700

    bt2

--- src/ss3_java_overview/Age.java
+++ src/ss3_java_overview/Age.java
@@ -4,16 +4,16 @@ import java.time.Year;
 import java.util.Scanner;
 
 public class Age {
+    private static int year;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Nhap vao nam sinh cua ban: ");
-        int year = scanner.nextInt();
+        int year = Integer.parseInt((scanner.nextLine()));
 
         int age = Year.now().getValue() - year;
 
         System.out.println("Tuoi cua ban la: " + age);
-
-
     }
 }
--- src/ss3_java_overview/Math.java
+++ src/ss3_java_overview/Math.java
@@ -6,5 +6,23 @@ public class Math {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+//        B1: Nhap vao hai so nguyen a va b tu ban phim
+        System.out.print("Nhap so a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Nhap so b: ");
+        int b = scanner.nextInt();
+
+//        B2-5: Tinh tong, hieu, tich va thuong cua a va b
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = (double) a / b; // Chuyen doi a sang double de chia chinh xac
+
+//        B6: In ra ket qua
+        System.out.println("Tong: " + sum);
+        System.out.println("Hieu: " + difference);
+        System.out.println("Tich: " + product);
+        System.out.println("Thuong: " + quotient);
     }
 }
