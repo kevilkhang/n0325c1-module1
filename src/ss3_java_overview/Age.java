3fc8e047f21b59d90d224eac60f048af418cd429
kevilkhang <kevilkhang@gmail.com>
2025-03-06 18:22:38 +0700

    update

--- src/ss3_java_overview/Age.java
+++ src/ss3_java_overview/Age.java
@@ -3,17 +3,20 @@ package ss3_java_overview;
 import java.time.Year;
 import java.util.Scanner;
 
+/**
+ * Nhap nam sinh cua mot nguoi. Tinh tuoi nguoi do
+ */
 public class Age {
-    private static int year;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Nhap vao nam sinh cua ban: ");
         int year = Integer.parseInt((scanner.nextLine()));
 
+        // Tinh tuoi
         int age = Year.now().getValue() - year;
 
+        // In ra ket qua
         System.out.println("Tuoi cua ban la: " + age);
     }
 }
